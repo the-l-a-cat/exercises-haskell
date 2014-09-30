57141a611fd10efd1dbb8b7bbe7d9a3d0fe2dedf
Nikita Shmyrev <the.l.a.cat@gmail.com>
2014-09-30 12:55:53 +0400

    Introducing Network.HTTP.Conduit and https fetch support.

--- Main.hs
+++ Main.hs
@@ -1,14 +1,20 @@
 module Main where
 
-import Control.Monad
+import Control.Monad 
+import Data.ByteString.Lazy
 
-import Network.HTTP
+import Network.HTTP.Conduit
 import Text.XML.HXT.Core
 
 main :: IO ()
-main = Network.HTTP.simpleHTTP (getRequest "http://www.haskell.org/")
-    >>= Network.HTTP.getResponseBody
-    >>= runX . xshow . readString [withParseHTML yes]
-    >>= mapM_ putStrLn
+main  = simpleHttp "https://hackage.haskell.org/packages/top"
+    >>= runX . xshow . (>>> multi (hasName "a"))
+      . readString [withParseHTML yes, withWarnings no]
+      . unchar8
+    >>= mapM_ Prelude.putStrLn
+
+    where
+        unchar8 = Prelude.map (toEnum . fromEnum) . unpack
+
 
 
