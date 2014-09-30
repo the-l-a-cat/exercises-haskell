6493252aee7b5de56a6912a45bd61ac578ae860f
Nikita Shmyrev <the.l.a.cat@gmail.com>
2014-09-30 10:13:28 +0400

    (A different way to) Print web page contents.

--- Main.hs
+++ Main.hs
@@ -1,17 +1,14 @@
 module Main where
 
+import Control.Monad
+
 import Network.HTTP
 import Text.XML.HXT.Core
 
 main :: IO ()
-main = do
-    rsp <- Network.HTTP.simpleHTTP (getRequest "http://www.haskell.org/")
-    -- res <- fmap (take 1) (Network.HTTP.getResponseBody rsp)
-    corpus <- Network.HTTP.getResponseBody rsp
-    let xmltree = readString [withParseHTML yes] corpus
-    text <- (runX . xshow) xmltree
-    mapM_ putStrLn text
-
-
+main = Network.HTTP.simpleHTTP (getRequest "http://www.haskell.org/")
+    >>= Network.HTTP.getResponseBody
+    >>= runX . xshow . readString [withParseHTML yes]
+    >>= mapM_ putStrLn
 
 
