d6082e500065720eeec74ef54c530f1f9d626d96
Nikita Shmyrev <the.l.a.cat@gmail.com>
2014-10-03 08:03:32 +0400

    Get and print (package name, counter) list from Hackage.
    1. Finding myself unable to wrap my mind around IO binds, I had to resort to do-notation again.
    2. I restructured function graph; it seems to look simpler and more logical now.

--- Main.hs
+++ Main.hs
@@ -7,19 +7,33 @@ import Network.HTTP.Conduit
 import Text.XML.HXT.Core
 
 main :: IO ()
-main =  parse target
-    >>= runX . (<<<) transform
-    >>= mapM_ putStrLn . take 5
+main = do
+    page <- parse url
+    links <- runX $ getContentsFromPath pathToLinks <<< page
+    counters <- runX $ getContentsFromPath pathToCounters <<< page
+    (mapM_ putStrLn . take 5) (formatReport links counters)
+        where
+        formatReport names numbers = map formatReportLine (zip names numbers)
+        formatReportLine (name, number) = name ++ " \t -> \t " ++ number
+
+        
 
 parse = liftM (readString [withParseHTML yes, withWarnings no] . unchar8 ) . simpleHttp
     where
     unchar8 = map (toEnum . fromEnum) . L.unpack
 
-target = "https://hackage.haskell.org/packages/top"
+url            = "https://hackage.haskell.org/packages/top"
+pathToLinks    = ["html", "body", "div", "div", "table", "tr", "td", "a"]
+pathToCounters = ["html", "body", "div", "div", "table", "tr", "td"]
 
-transform = walk ["html", "body", "div", "div", "table", "tr", "td"]
-        >>> getChildren
-        >>> removeWhiteSpace
-        >>> getText
+getContentsFromPath path = walk path >>> getChildren >>> getText
+
+
+-- transform = foldr (>>>) returnA
+--                 [       walk ["html", "body", "div", "div", "table", "tr", "td"]
+--                     -- &&& walk ["html", "body", "div", "div", "table", "tr", "td", "a"]
+--                     -- <+> walk ["html", "body", "div", "div", "table", "tr", "td", "a"]
+--                 , getChildren
+--                 , removeWhiteSpace ]
 
 walk tags = foldr (>>>) returnA [ getChildren >>> isElem >>> hasName tag | tag <- tags ]
