588da3f503e1746350297e5c1734f286c0bf99c2
Nikita Shmyrev <the.l.a.cat@gmail.com>
2014-10-05 05:48:16 +0400

    Rewriting "walk" function. The new one is "walkA".
    1. "walkA" takes a list of "Tag" objects rather than just strings.
    2. "Tag" object is a name and a list of attributes.
    3. I think we may now write a lexer to transform a string to a list of "Tag" objects
        to parametrize the function with.
    
    All of this should allow for more precision with ease of use on top.

--- Main.hs
+++ Main.hs
@@ -5,18 +5,20 @@ import Control.Arrow
 import qualified Data.ByteString.Lazy as L 
 import Network.HTTP.Conduit
 import Text.XML.HXT.Core
+import Data.List
 
 main :: IO ()
 main = do
     page <- parse url
     links <- runX $ getContentsFromPath pathToLinks <<< page
     counters <- runX $ getContentsFromPath pathToCounters <<< page
-    printResult' (formatReport links counters)
+    let lines = zip links counters
+    print =<< (runX $ page >>> walkA tagPathSomewhere)
+    -- printResult [formatReportLine line | line <- lines, "oauth" `isInfixOf` (fst line) ]
         where
         formatReport names numbers = map formatReportLine (zip names numbers)
         formatReportLine (name, number) = name ++ " \t -> \t " ++ number
-        printResult = mapM_ putStrLn . take 5
-        printResult' = print
+        printResult = mapM_ putStrLn
 
         
 
@@ -24,9 +26,11 @@ parse = liftM (readString [withParseHTML yes, withWarnings no] . unchar8 ) . sim
     where
     unchar8 = map (toEnum . fromEnum) . L.unpack
 
-url            = "https://hackage.haskell.org/packages/top"
+url            = "https://g.co" 
 pathToLinks    = ["html", "body", "div", "div", "table", "tr", "td", "a"]
 pathToCounters = ["html", "body", "div", "div", "table", "tr", "td"]
+
+tagPathSomewhere = [ Tag "html" [], Tag "body" [], Tag "div" [ TagAttr "class" "maia-header" ] ]
 
 getContentsFromPath path = walk path >>> getChildren >>> getText
 
@@ -39,3 +43,16 @@ getContentsFromPath path = walk path >>> getChildren >>> getText
 --                 , removeWhiteSpace ]
 
 walk tags = foldr (>>>) returnA [ getChildren >>> isElem >>> hasName tag | tag <- tags ]
+
+
+type TagAttrs = [ TagAttr ]
+data TagAttr = TagAttr { attrName :: String, attrValue :: String }
+data Tag = Tag { tagName :: String, tagAttrs :: TagAttrs }
+
+walkA :: ArrowXml cat0 => [Tag] -> cat0 XmlTree XmlTree
+walkA tags = foldr (>>>) returnA [ getChildren >>> isElem >>> tagTest tag | tag <- tags ]
+    where
+    tagTest tag = hasName (tagName tag) >>> foldr (>>>) returnA
+        [ hasAttrValue (attrName attr) (== attrValue attr) | attr <- tagAttrs tag ]
+        
+
